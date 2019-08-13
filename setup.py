d95bb3bdffc04f245388bb0ae75f60e10eff588b
philippe.docourt <philippe@docourt.ch>
2019-08-13 22:18:07 +0200

    Added setup for PyPI packaging.

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-string-renderer',
+    version='0.1.1',
+    url='https://github.com/dprog-philippe-docourt/django-string-renderer',
+    license='MIT',
+    author='Philippe Docourt',
+    author_email='contact@dprog.net',
+    description='A thin wrapper around the Django templating system to render any string as a template. It provides an easy way to render any user inputted string as a regular django template.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.5',
+    install_requires=['django'],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English"
+    ],
+    keywords='templating django',
+)
