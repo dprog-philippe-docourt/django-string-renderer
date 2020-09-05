dc0aa069328e42583fcfedb0c4ae6549788a43b7
philippe.docourt <philippe@docourt.ch>
2020-09-05 18:15:30 +0200

    Prepare release 0.2.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='django-string-renderer',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/dprog-philippe-docourt/django-string-renderer',
     license='MIT',
     author='Philippe Docourt',
