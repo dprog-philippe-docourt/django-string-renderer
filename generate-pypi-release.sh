9da3cccc3a07407c1d2f799508abf81d0792d8bc
philippe.docourt <philippe@docourt.ch>
2022-11-05 23:02:54 +0100

    Update generate-pypi-release.sh.

--- generate-pypi-release.sh
+++ generate-pypi-release.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 (
-    cd stringrenderer && python ../manage.py compilemessages && cd ..
     python -m pip install --upgrade pip
-    pip install --upgrade setuptools wheel twine
+    pip install -r requirements.txt
+    pip install --upgrade -r requirements-dev.txt
+    cd stringrenderer && python ../manage.py compilemessages && cd ..
     rm -r build/ dist/ *.egg-info/
     python setup.py check && python setup.py sdist && python setup.py bdist_wheel && twine upload dist/*
 )
